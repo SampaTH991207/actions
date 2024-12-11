5cac12bfab8edaff52407ac067adf8fbf6926f13
SampaTH990712 <sampathkumar.ramgiri@valpo.edu>
2024-12-11 00:19:59 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World! This is the app for testing the .NET application.";
+            PrintMessage(message);
+        }
+
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine();
+        }
+    }
+}
+
